66a64736e6af90c048901b559f35fd132871e6c8
KeshavKulhar <82043409+KeshavKulhar@users.noreply.github.com>
2021-10-17 13:34:59 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,19 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD_UqWNiPjzcnwSaBluRwNLsDLyLV3bHF8",
+    authDomain: "c-93-project.firebaseapp.com",
+    projectId: "c-93-project",
+    storageBucket: "c-93-project.appspot.com",
+    messagingSenderId: "916679728213",
+    appId: "1:916679728213:web:b41827fdda94c6cc2548dc"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
